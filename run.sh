a1992fb4f07ce7319024c64023a872ece782c8ea
jojuo123 <vtduy3100@gmail.com>
2021-08-09 23:41:26 +0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-# python3.7 -m code.cmds.ensemble_crf2o train -b -d 0  \
-# -c config.ini  \
-# -p "exp2/add_ptb_train" \
-# -f bert \
-# --bert "vinai/phobert-base" \
-# --epochs 1000 \
-# --train "code/data/add_ptb/vi_train.conllx" \
-# --dev "code/data/add_ptb/vi_dev.conllx" \
-# --test "code/data/add_ptb/vi_test.conllx" \
-# --train_add "code/data/add_ptb/en_full.conllx" \
-# --batch-size 5000 \
-# --mbr \
-# --use_cpos
+python3.7 -m code.cmds.ensemble_dependency train -b -d 0  \
+-c config.ini  \
+-p "exp3/test_trans_encode" \
+-f bert \
+--bert "vinai/phobert-base" \
+--epochs 1000 \
+--train "code/data/add_ptb/vi_train.conllx" \
+--dev "code/data/add_ptb/vi_dev.conllx" \
+--test "code/data/add_ptb/vi_test.conllx" \
+--train_add "code/data/add_ptb/en_full.conllx" \
+--batch-size 5000 \
+--tag_set_path "code/data/pos_set.txt" \
+--use_cpos
 
-python3.7 -m code.cmds.ensemble_dependency evaluate \
---data "./code/data/ptb/dev.conllu" \
--p "./exp2/normal_2_thread_nobert" \
--c config.ini \
--d 0
+# python3.7 -m code.cmds.ensemble_dependency evaluate \
+# --data "./code/data/ptb/dev.conllu" \
+# -p "./exp2/normal_2_thread_nobert" \
+# -c config.ini \
+# -d 0
